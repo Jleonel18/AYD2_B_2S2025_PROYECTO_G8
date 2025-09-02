6bb0b1e16cbca18f0787e02f4cd0449925239812
Jleonel18 <lopezajvixjoseleonel@gmail.com>
2025-09-02 13:41:07 -0600

    202201211: feat(frontend): Se hizo la vista del perfil del usuario

--- Fase 2/frontend/src/App.jsx
+++ Fase 2/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Login from './Pages/login'
 import Register from './Pages/register'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProfileUser from './Pages/profileUser'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<ProfileUser />} />
       </Routes>
     </BrowserRouter>
   )
