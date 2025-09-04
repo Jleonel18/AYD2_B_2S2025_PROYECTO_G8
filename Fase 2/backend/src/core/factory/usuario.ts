4c2150fd3fb98d7206604f27602e9bd9fe2f6c89
Jose Gongora <jdavidgongorao@gmail.com>
2025-09-03 23:13:46 -0600

    202201444: feat(backend): Se arreglo el registro de usuarios roles

--- Fase 2/backend/src/core/factory/usuario.ts
+++ Fase 2/backend/src/core/factory/usuario.ts
@@ -10,7 +10,15 @@ abstract class Usuario {
         public readonly nombre: string,
         public readonly edad: number,
         public readonly correo: string,
-        public readonly tipo: UsuarioType
+        public readonly telefono: string,
+        public readonly direccion: string,
+        public readonly genero: string,
+        public readonly fecha_nacimiento: Date,
+        public readonly dpi: string,
+        public readonly usuario: string,
+        public readonly contrasena: string,
+        public readonly tipo: UsuarioType,
+        public readonly vuelos: string[] = []
     ) {}
     abstract toJSON(): Record<string, unknown>
 }
