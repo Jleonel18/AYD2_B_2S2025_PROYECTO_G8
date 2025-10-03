ee2d401b31d99c60398ad936e9ac0d21c69a77c8
Jleonel18 <lopezajvixjoseleonel@gmail.com>
2025-10-03 11:27:05 -0600

    202201211: feat(frontend): Se hizo un cambio en las rutas de Cypress

--- Fase 3/frontend/cypress.config.js
+++ Fase 3/frontend/cypress.config.js
@@ -5,6 +5,9 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:5173', // Ajustado a tu puerto de Vite
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
     env: {
       apiURL: 'http://localhost:3000/api',
     },
